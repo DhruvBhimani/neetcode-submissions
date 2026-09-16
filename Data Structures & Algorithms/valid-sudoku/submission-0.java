5e85d25723c782d7a3389abf48ec91f92a090560
Dhruv Bhimani <86870161+DhruvBhimani@users.noreply.github.com>
2026-09-15 17:43:11 -0700

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,68 @@
+class Solution 
+{
+    public boolean isValidSudoku(char[][] board) 
+    {
+        
+        Set<Character> row = new HashSet<>();
+        Set<Character> column = new HashSet<>();
+       for(int i=0; i<9; i++)
+       {
+            for(int j=0; j<9; j++)
+            {
+                if(board[i][j]!= '.')
+                {
+                    if(!row.contains(board[i][j]))
+                    {
+                        row.add(board[i][j]);
+                    }
+                    else if(row.contains(board[i][j]))
+                    {
+                        return false;
+                    }
+                    else if(!column.contains(board[j][i]))
+                    {
+                        column.add(board[j][i]);
+                    }
+                    else if(column.contains(board[j][i]))
+                    {
+                        return false;
+                    }
+                }
+                 
+            } 
+            row.clear();
+            column.clear();
+       } 
+
+       
+        for(int i=0; i<9; i=i+3)
+       {
+            for(int j=0; j<9; j=j+3)
+            {
+                for(int a=0; a<3; a++)
+                {
+                    for(int b=0; b<3; b++)
+                    {
+                        if(board[i+a][j+b]!= '.')
+                        {
+                            if(!row.contains(board[i+a][j+b]))
+                            {
+                                row.add(board[i+a][j+b]);
+                            }
+                            else if(row.contains(board[i+a][j+b]))
+                            {
+                                return false;
+                            }
+                        }
+                        
+                    } 
+                }
+                row.clear();
+            }
+            
+       } 
+
+
+       return true;
+    }
+}
